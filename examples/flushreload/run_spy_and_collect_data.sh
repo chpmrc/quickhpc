6d225698427e8288a57574d04f1c2872db365ad0
mardurhack <mardurhack@gmail.com>
2015-04-04 16:57:12 +0300

    Add example to monitor spy and victim (FLUSH+RELOAD)

---
+++ examples/flushreload/run_spy_and_collect_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Needed directories
+VICTIM_DIR="${HOME}/Projects/flushreload_yarom/openssl/"
+SPY_DIR="${HOME}/Projects/flushreload_yarom/spy/"
+CLASSIF_DIR="${HOME}/Projects/malware_classifier/"
+MONITOR_DIR="${HOME}/Projects/hpc_highres_monitor/"
+OPENSSL_DIR="${HOME}/Projects/flushreload_yarom/openssl"
+
+cd ${OPENSSL_DIR}
+
+${SPY_DIR}/spy ${OPENSSL_DIR}/openssl.probe > /dev/null & 
+${MONITOR_DIR}/monitor -a ${!} -c ${MONITOR_DIR}/conf/events_example.conf > ${CLASSIF_DIR}/data/spy_data.csv & 
+${VICTIM_DIR}/openssl-1.0.1e/apps/openssl dgst -sha1 -sign ${VICTIM_DIR}/test_data/private.pem ${VICTIM_DIR}/test_data/test.pdf > /dev/null & 
+${MONITOR_DIR}/monitor -a ${!} -c ${MONITOR_DIR}/conf/events_example.conf > ${CLASSIF_DIR}/data/victim_data.csv; killall spy
+
